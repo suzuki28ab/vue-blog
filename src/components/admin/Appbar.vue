075ffa17d3a2251239219e57e5cc1d44721b1ffa
ryo28ab <suzuki28ab@gmail.com>
2020-03-26 03:58:29 +0900

    [add]fontawesome

--- src/components/admin/Appbar.vue
+++ src/components/admin/Appbar.vue
@@ -1,12 +1,28 @@
 <template>
   <div>
-    <v-app-bar app clipped-left>
+    <v-app-bar app clipped-left color="primary">
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
     </v-app-bar>
     <v-navigation-drawer app clipped v-model="drawer" mobile-break-point="960">
-      <v-layout justify-center>
-        <p>test</p>
-      </v-layout>
+      <v-container>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title class="title">設定項目</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-divider></v-divider>
+
+        <v-list nav>
+          <v-list-item v-for="navList in navLists" :key="navList.name">
+            <v-list-item-icon>
+              <v-icon small>{{ navList.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>{{ navList.name }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-container>
     </v-navigation-drawer>
   </div>
 </template>
@@ -16,7 +32,8 @@ import Vue from "vue";
 export default Vue.extend({
   name: "Header",
   data: () => ({
-    drawer: null
+    drawer: null,
+    navLists: [{ name: "投稿", icon: "fab fa-blogger-b" }]
   })
 });
 </script>
