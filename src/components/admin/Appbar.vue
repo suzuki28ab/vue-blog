bc22735d81f61085df997859b5ef95710eae608c
ryo28ab <suzuki28ab@gmail.com>
2020-03-25 23:22:20 +0900

    [add]admin component

---
+++ src/components/admin/Appbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <v-app-bar app clipped-left>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+    </v-app-bar>
+    <v-navigation-drawer app clipped v-model="drawer" mobile-break-point="960">
+      <v-layout justify-center>
+        <p>test</p>
+      </v-layout>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "Header",
+  data: () => ({
+    drawer: null
+  })
+});
+</script>
